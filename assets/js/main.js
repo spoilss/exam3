c5e250b58c8f3895ddc55c6246765d7284edbf06
spoilss <spoilssbor@gmail.com>
2017-02-11 19:48:59 +0200

    exam3

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	$('.recent-slider').slick({
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		adaptiveHeight: true,
+		arrow:true,
+		centerMode: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+	});
+	
+});
